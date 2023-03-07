e1b65218cf2d600f3de07d7b9c9536d720cad990
abirahmed56 <abirahmedsohan6@gmail.com>
2023-03-07 17:07:25 +0600

    mongodb database added

--- pages/api/books.js
+++ pages/api/books.js
@@ -1,28 +1,32 @@
-import fs from "fs";
-import path from "path";
+import mongodb, { MongoClient } from "mongodb";
 
-function getData() {
-  const filePath = path.join(process.cwd(), "data", "books.json");
-  const fileData = fs.readFileSync(filePath);
-  const data = JSON.parse(fileData);
+async function handler(req, res) {
+  const client = await MongoClient.connect(
+    "mongodb+srv://Abir:nwlTKRX6Tvq96q9o@cluster0.a9jq4mu.mongodb.net/?retryWrites=true&w=majority"
+  );
 
-  return data;
-}
-function handler(req, res) {
+  //create database
+
+  const db = client.db("books");
+
   if (req.method === "GET") {
-    const data = getData();
-    return res.status(200).json({ message: data });
-  } else if (req.method == "POST") {
+    const books = await db.collection("books").find().sort().toArray();
+
+    if (!books) {
+      return res.status(500).json({ message: "internal Server Error" });
+    }
+    return res.status(200).json({ message: books });
+  } else if (req.method === "POST") {
     const { name, description, imgUrl } = req.body;
 
-    const newBook = { name, description, imgUrl, id: Date.now() };
-    const filePath = path.join(process.cwd(), "data", "books.json");
-    const data = getData();
-    data.push(newBook);
-    fs.writeFileSync(filePath, JSON.stringify(data))
-    return res.status(200).json({ message: "Added", book: newBook });
-  }
-  
-}
+    const newBook = {
+      name,
+      description,
+      imgUrl,
+    };
 
+    const res2 = await db.collection("books").insertOne(newBook)
+    return res.status(201).json({ message: "Added", book: newBook });
+  }
+}
 export default handler;
